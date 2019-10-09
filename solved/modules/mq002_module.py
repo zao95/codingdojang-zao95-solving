7f99efbbcfdba8b76ce4939aa34e2de387559ef4
Jeongwoo <hyzzang125@gmail.com>
2019-10-10 02:21:29 +0900

    update mq002

--- solved/modules/mq002_module.py
+++ solved/modules/mq002_module.py
@@ -25,9 +25,7 @@ class Sort:
         for i in range(0, len(sequential_list_1)):
             for j in range(i+1, len(sequential_list_1)):
                 if sequential_list_1[i] > sequential_list_1[j]:
-                    temp = sequential_list_1[i]
-                    sequential_list_1[i] = sequential_list_1[j]
-                    sequential_list_1[j] = temp
+                    sequential_list_1[i], sequential_list_1[j] = sequential_list_1[j], sequential_list_1[i]
                     change_count += 1
         # end timewatch
         sequential_1_endtime = time.time()
@@ -57,9 +55,7 @@ class Sort:
         for i in range(len(bubble_list_1), 0, -1):
             for j in range(0, i-1):
                 if bubble_list_1[j] > bubble_list_1[j+1]:
-                    temp = bubble_list_1[j]
-                    bubble_list_1[j] = bubble_list_1[j+1]
-                    bubble_list_1[j+1] = temp
+                    bubble_list_1[j], bubble_list_1[j+1] = bubble_list_1[j+1], bubble_list_1[j]
                     change_count += 1
 
         # end timewatch
@@ -94,9 +90,7 @@ class Sort:
             for j in range(i+1, len(selection_list_1)):
                 temp_list.append(selection_list_1[j])
                 if selection_list_1[i] > min(temp_list):
-                    temp = selection_list_1[i]
-                    selection_list_1[i] = selection_list_1[j]
-                    selection_list_1[j] = temp
+                    selection_list_1[i], selection_list_1[j] = selection_list_1[j], selection_list_1[i]
                     change_count += 1
         # end timewatch
         selection_endtime = time.time()
@@ -111,4 +105,34 @@ class Sort:
             print("정상작동 실패")
             print("차이 내용")
             print(selection_list_1)
+            print(self.right_answer)
+ 
+
+    # insertion sort #1 sort
+    def insertion_1(self):
+        # copy random list
+        insertion_list_1 = copy.deepcopy(self.mother_list)
+        # start timewatch
+        insertion_starttime = time.time()
+        # count reset
+        change_count = 0
+        # sort
+        for i in range(0, len(insertion_list_1)):
+            while 0 < i and insertion_list_1[i] < insertion_list_1[i - 1]: 
+                insertion_list_1[i], insertion_list_1[i - 1] = insertion_list_1[i - 1], insertion_list_1[i]
+                i -= 1
+                change_count += 1
+        # end timewatch
+        insertion_endtime = time.time()
+        # insertion #1 result
+        print("==========")
+        print("삽입정렬 #1 완료")
+        print("소요시간 {time}초".format(time = insertion_endtime - insertion_starttime))
+        if(insertion_list_1 == self.right_answer):
+            print("정상작동 확인")
+            print("교체 횟수: {times}회".format(times = change_count))
+        else:
+            print("정상작동 실패")
+            print("차이 내용")
+            print(insertion_list_1)
             print(self.right_answer)
